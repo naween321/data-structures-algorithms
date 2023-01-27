46c177659b70fa4ca0f6a868eb9c0ca48bde9a7a
npaudyal321 <naween321@gmail.com>
2023-01-27 23:36:51 +0545

    selection sort for repeated numbers

--- sorting_algorithms/selection_sort.py
+++ sorting_algorithms/selection_sort.py
@@ -1,6 +1,6 @@
-lis = [56, 3, 2, 78]
+lis = [56, 3, 2, 78, 3]
 for i in range(len(lis)):
     min_val = min(lis[i:])
-    min_ind = lis.index(min_val)
+    min_ind = lis.index(min_val, i)
     lis[i], lis[min_ind] = lis[min_ind], lis[i]
 print(lis)
