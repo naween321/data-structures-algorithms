94885ccc1a0e376b9eb1ca9dacf9d53d872f714c
npaudyal321 <naween321@gmail.com>
2023-01-28 23:55:44 +0545

    binary search complete

--- searching_algorithm/binary_search.py
+++ searching_algorithm/binary_search.py
@@ -1,1 +1,22 @@
+def binary_search(list1, key):
+    low = 0
+    high = len(list1) - 1
+    found = False
+    while low <= high and not found:
+        mid = (low + high) // 2
+        if key == list1[mid]:
+            found = True
+        elif key > list1[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    if found:
+        print("Key found")
+    else:
+        print("Key not found")
+
+
 list1 = [23, 1, 4, 2, 3]
+list1.sort()
+key = int(input("Enter search value: "))
+binary_search(list1, key)
