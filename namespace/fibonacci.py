e62231b4cc745127bb55bfdfe814e0c1a542b6a7
npaudyal321 <naween321@gmail.com>
2023-02-01 09:03:38 +0545

    recusrion fibonacci

--- namespace/fibonacci.py
+++ namespace/fibonacci.py
@@ -1,13 +1,27 @@
-def fibonacci():
-    a = 0
-    b = 1
-    while True:
-        c = a
-        a = b
-        b = a + c
-        yield c
+# def fibonacci():
+#     a = 0
+#     b = 1
+#     while True:
+#         c = a
+#         a = b
+#         b = a + c
+#         yield c
+#
+#
+# f = fibonacci()
+# for _ in range(20):
+#     print(next(f))
+
+# With recursion
 
 
-f = fibonacci()
-for _ in range(20):
-    print(next(f))
+def fib(a, b, count):
+    print(a)
+    c = a + b
+    count += 1
+    if count == 20:
+        return
+    return fib(b, c, count)
+
+
+fib(0, 1, 0)
