f40f75734aa36538d7c11323fc07d7e03f4eef01
npaudyal321 <naween321@gmail.com>
2023-02-11 09:18:59 +0545

    add exception log

--- log/sample.py
+++ log/sample.py
@@ -1,14 +1,16 @@
 import logging
-import employee
-logging.basicConfig(level=logging.DEBUG, filename='test.log', format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(level=logging.INFO, filename='test.log', format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def add(n1, n2):
-    return n1 + n2
+    try:
+        return n1 + n2
+    except TypeError:
+        logging.exception("Type Difference")
 
 
 num1 = 10
-num2 = 5
+num2 = "abc"
 
 add_result = add(num1, num2)
 logging.debug(f"{num1} + {num2} = {add_result}")
