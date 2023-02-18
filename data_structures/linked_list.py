4e533d876deadce90620e87582543f407c2a55ef
npaudyal321 <naween321@gmail.com>
2023-02-18 22:05:56 +0545

    adds linked list

---
+++ data_structures/linked_list.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+l = LinkedList()
+l.head = Node(1)
+second = Node(2)
+third = Node(3)
+l.head.next = second
+second.next = third
+while l.head != None:
+    print(l.head.value, end=" ")
+    l.head = l.head.next
